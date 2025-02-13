25073304dcf122231f40f17ae547cf6ce05450b1
tanujburse <tanujburse@gmail.com>
2025-02-13 22:46:40 +0530

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+<a name="19.0.0"></a>
+
+# 19.0.0 (2025-01-29)
+
+### app
+
+| Type | Description                                    |
+| -- |------------------------------------------------|
+| feat | add number of real time users inside home page |
+
+---
---
+++ playwright.config.ts
@@ -0,0 +1,46 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './e2e',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: false,
+  forbidOnly: true,
+  retries: 2,
+  workers: 1,
+  reporter: 'html',
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'Google Chrome',
+      use: {
+        channel: 'chrome',
+      },
+    },
+    {
+      name: 'Safari',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 12'],
+      },
+    },
+  ],
+};
+
+export default config;
